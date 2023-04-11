bf779ad0d8aa69f5cd40dab370f8bd82f47430af
lybcos <1934428475@qq.com>
2023-04-11 17:28:18 +0800

    首次提交——多数据源比较数据差异及积分消费情况

---
+++ src/main/java/com/demo/mgz/MgzApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mgz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/mgz/MgzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.mgz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
